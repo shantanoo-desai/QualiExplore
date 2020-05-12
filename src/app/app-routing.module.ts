5e6fdeb271f44cc03e94c95334c05554dbef6286
Shantanoo Desai <des@biba.uni-bremen.de>
2020-05-12 12:44:31 +0200

    chore(app): set routes for qualiexplore
    - Upon hitting the base (`/`) reroute to path `qualiexplore`
    - Use Lazy-Loading for Qualiexplore

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/qualiexplore', pathMatch: 'full'
+  },
+  {
+    path: 'qualiexplore',
+    loadChildren: './qualiexplore/qualiexplore.module#QualiexploreModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
